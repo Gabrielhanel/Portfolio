5c4bbcea873553525796b301988b0fe58c19c7f3
Gabriel <ghanel527@gmail.com>
2024-09-15 15:13:12 -0300

    Projeto final

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000, // Tempo de rolagem em milissegundos
     easing: 'easeInOutCubic' // Tipo de easing
 });
+
+// Adicionar um evento de clique em cada item do menu para fechar o menu
+const menuItems = document.querySelectorAll('.menu a');
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('show');   
+
+    });
+});
